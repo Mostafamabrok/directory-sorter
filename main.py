2fae6dc7b2e1b0e1d6510681147d610433137c4c
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2021-02-14 13:16:55 +0300

    Modified the loop in which the files are sorted by making the move() function which replaces shutil.move(os.path.join(directory, filename), os.path.join(target, filename))

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 directory = input("Folder you want to organize, Full path please: ")
 targets = [os.path.join(directory,"pdfs"), os.path.join(directory,"Pictures"), os.path.join(directory,"worddocs"), os.path.join(directory,"zipfiles"), os.path.join(directory,"misc"), os.path.join(directory,"txtfiles")]
 
+def move():
+    shutil.move(os.path.join(directory, filename), os.path.join(target, filename))
+
 for paths in targets:
     if not os.path.isdir(paths):
         os.mkdir(paths)
@@ -12,23 +15,23 @@ for filename in os.listdir(directory):
         
     if filename.endswith(".zip"):
         target = targets[3]
-        shutil.move(os.path.join(directory, filename), os.path.join(target, filename))
+        move()
         
     if filename.endswith(".png") or filename.endswith(".jpg"):
         target = targets[1]
-        shutil.move(os.path.join(directory, filename), os.path.join(target, filename))
+        move
         
     if filename.endswith(".pdf"):
         target = targets[0]
-        shutil.move(os.path.join(directory, filename), os.path.join(target, filename))
+        move()
         
     if filename.endswith(".docx"):
         target = targets[2]
-        shutil.move(os.path.join(directory, filename), os.path.join(target, filename))
+        move
         
     if filename.endswith(".txt"):
         target = targets[5]
-        shutil.move(os.path.join(directory, filename), os.path.join(target, filename))    
+        move()    
         
     if os.path.isdir(os.path.join(directory, filename)):
         continue
