c59f27f8be606b44f31050968ae70133601023c8
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2021-02-17 08:51:03 +0300

    Added comments to main.py for clarity

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import shutil
 import os
 
+#This takes the directory the user wants to organize as input
 directory = input("Folder you want to organize, Full path please: ")
+
 targets = [os.path.join(directory,"pdfs"), os.path.join(directory,"Pictures"), os.path.join(directory,"worddocs"), os.path.join(directory,"zipfiles"), os.path.join(directory,"misc"), os.path.join(directory,"txtfiles")]
 
+#This function is just to recude code duplication and it moves <filename> to the target directory using the shutil module 
 def move():
     shutil.move(os.path.join(directory, filename), os.path.join(target, filename))
 
+#The loop that checks that the folders that <filename> will be sorted into exist
 for paths in targets:
     if not os.path.isdir(paths):
         os.mkdir(paths)
 
+#The actual loop that sorts the files
 for filename in os.listdir(directory):
         
     if filename.endswith(".zip"):
@@ -32,7 +37,7 @@ for filename in os.listdir(directory):
     if filename.endswith(".txt"):
         target = targets[5]
         move()    
-        
+    #This is for sorting into the misc folder    
     if os.path.isdir(os.path.join(directory, filename)):
         continue
     elif os.path.isfile(os.path.join(directory, filename)):
