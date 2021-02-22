2b122e1a11643286d2d25358045cbb2e3d974e8c
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2021-02-22 09:59:10 +0300

    Added support for .csv and .xlsx files and made the code to move those files into data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 #This takes the directory the user wants to organize as input
 directory = input("Folder you want to organize, Full path please: ")
 
-targets = [os.path.join(directory,"pdfs"), os.path.join(directory,"Pictures"), os.path.join(directory,"worddocs"), os.path.join(directory,"zipfiles"), os.path.join(directory,"misc"), os.path.join(directory,"txtfiles")]
+targets = [os.path.join(directory,"pdfs"), os.path.join(directory,"Pictures"), os.path.join(directory,"worddocs"), os.path.join(directory,"zipfiles"), os.path.join(directory,"misc"), os.path.join(directory,"txtfiles"), os.path.join(directory,"data")]
 
 #This function is just to recude code duplication and it moves <filename> to the target directory using the shutil module 
 def move():
@@ -36,7 +36,12 @@ for filename in os.listdir(directory):
         
     if filename.endswith(".txt"):
         target = targets[5]
-        move()    
+        move()
+    
+    if filename.endswith(".csv") or filename.endswith(".xslx"):
+        target = targets[6]
+        move()
+        
     #This is for sorting into the misc folder    
     if os.path.isdir(os.path.join(directory, filename)):
         continue
