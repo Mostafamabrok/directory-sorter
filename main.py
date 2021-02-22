738da329178bf7ac4b974a3982aef15eb0d4bbed
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2021-02-22 14:22:31 +0300

    Ordered the if statements that move filename

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 #This takes the directory the user wants to organize as input
 directory = input("Folder you want to organize, Full path please: ")
 
-targets = [os.path.join(directory,"pdfs"), os.path.join(directory,"Pictures"), os.path.join(directory,"worddocs"), os.path.join(directory,"zipfiles"), os.path.join(directory,"misc"), os.path.join(directory,"txtfiles"), os.path.join(directory,"data")]
+targets = [os.path.join(directory,"pdfs"), os.path.join(directory,"Pictures"), os.path.join(directory,"worddocs"), os.path.join(directory,"zipfiles"), os.path.join(directory,"txtfiles"), os.path.join(directory,"data"),os.path.join(directory,"misc")]
 
 #This function is just to recude code duplication and it moves <filename> to the target directory using the shutil module 
 def move():
@@ -17,36 +17,37 @@ for paths in targets:
 
 #The actual loop that sorts the files
 for filename in os.listdir(directory):
-        
-    if filename.endswith(".zip"):
-        target = targets[3]
-        move()
-        
-    if filename.endswith(".png") or filename.endswith(".jpg"):
-        target = targets[1]
-        move()
-        
+
     if filename.endswith(".pdf"):
         target = targets[0]
         move()
-        
+
+    if filename.endswith(".png") or filename.endswith(".jpg"):
+        target = targets[1]
+        move()
+
     if filename.endswith(".docx"):
         target = targets[2]
         move()
+
+    if filename.endswith(".zip"):
+        target = targets[3]
+        move()
+            
         
     if filename.endswith(".txt"):
-        target = targets[5]
+        target = targets[4]
         move()
     
     if filename.endswith(".csv") or filename.endswith(".xslx"):
-        target = targets[6]
+        target = targets[5]
         move()
         
     #This is for sorting into the misc folder    
     if os.path.isdir(os.path.join(directory, filename)):
         continue
     elif os.path.isfile(os.path.join(directory, filename)):
-        target = targets[4]
+        target = targets[6]
         shutil.move(os.path.join(directory, filename), os.path.join(target, filename))
 
         
